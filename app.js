f867ec84a58d641622e336573ebb4c41619728ee
America M <america-develops@gmail.com>
2021-12-16 08:24:04 -0500

    Complete code up through Section 2.12 of course.

--- app.js
+++ app.js
@@ -333,3 +333,34 @@
 
 
 // SECTION 2.12 - Template literals
+    const name = 'John';
+    const age = 31;
+    const job = 'Web Developer';
+    const city = 'Miami';
+    let html;
+    function hello() { return 'hello from function'; }
+
+    // Without template strings (es5)
+    html =  '<ul>' +
+                '<li>Name: ' + name + ' </li>' +
+                '<li>Age: ' + age + ' </li>' +
+                '<li>Job: ' + job + ' </li>' +
+                '<li>City: ' + city + ' </li>' +
+            '</ul>'
+    ;
+
+    // With template strings (es6)
+    html = `
+        <ul>
+            <li>Name: ${name}</li>
+            <li>Age: ${age}</li>
+            <li>Job: ${job}</li>
+            <li>City: ${city}</li>
+            <li>${2 + 2}</li>
+            <li>${hello()}</li>
+            <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
+        </ul>
+    `;
+    
+    // Output HTML
+    document.body.innerHTML = html;
