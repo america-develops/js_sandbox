420be9c76f9833767dda106c811a2a0b538a44d7
America M <america-develops@gmail.com>
2021-12-16 15:26:10 -0500

    Comment out sections before Section 2.12

--- app.js
+++ app.js
@@ -333,34 +333,78 @@
 
 
 // SECTION 2.12 - Template literals
-    const name = 'John';
-    const age = 31;
-    const job = 'Web Developer';
-    const city = 'Miami';
-    let html;
-    function hello() { return 'hello from function'; }
+    // const name = 'John';
+    // const age = 31;
+    // const job = 'Web Developer';
+    // const city = 'Miami';
+    // let html;
+    // function hello() { return 'hello from function'; }
 
-    // Without template strings (es5)
-    html =  '<ul>' +
-                '<li>Name: ' + name + ' </li>' +
-                '<li>Age: ' + age + ' </li>' +
-                '<li>Job: ' + job + ' </li>' +
-                '<li>City: ' + city + ' </li>' +
-            '</ul>'
-    ;
+    // // Without template strings (es5)
+    // html =  '<ul>' +
+    //             '<li>Name: ' + name + ' </li>' +
+    //             '<li>Age: ' + age + ' </li>' +
+    //             '<li>Job: ' + job + ' </li>' +
+    //             '<li>City: ' + city + ' </li>' +
+    //         '</ul>'
+    // ;
 
-    // With template strings (es6)
-    html = `
-        <ul>
-            <li>Name: ${name}</li>
-            <li>Age: ${age}</li>
-            <li>Job: ${job}</li>
-            <li>City: ${city}</li>
-            <li>${2 + 2}</li>
-            <li>${hello()}</li>
-            <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
-        </ul>
-    `;
+    // // With template strings (es6)
+    // html = `
+    //     <ul>
+    //         <li>Name: ${name}</li>
+    //         <li>Age: ${age}</li>
+    //         <li>Job: ${job}</li>
+    //         <li>City: ${city}</li>
+    //         <li>${2 + 2}</li>
+    //         <li>${hello()}</li>
+    //         <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
+    //     </ul>
+    // `;
     
-    // Output HTML
-    document.body.innerHTML = html;
+    // // Output HTML
+    // document.body.innerHTML = html;
+
+
+// SECTION 2.13 - Arrays & Array Methods
+    // Create some arrays
+    const numbers = [43,56,33,23,44,36,5];
+    const numbers2 = new Array(22,45,33,76,54);
+    const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
+    const mixed = [22, 'Hello', true, undefined, null, {a:1, b:1}, new Date()];
+
+    let val;
+
+    // Get array length
+    val = numbers.length;
+    // Check if is array
+    val = Array.isArray(numbers);
+    // Get single value
+    val = numbers[3];
+    val = numbers[0];
+    // Insert into array
+    numbers[2] = 100;
+    // Find index of value
+    val = numbers.indexOf(36);
+
+    // MUTATING ARRAYS
+    // Add on to end
+    numbers.push(250);
+    // Add on to front
+    numbers.unshift(120);
+    // Take off from end
+    numbers.pop();
+    // Take off from front
+    numbers.shift();
+    // Splice values
+    numbers.splice(1,3);
+    // Reverse
+    numbers.reverse();
+
+    // CONCATENATE ARRAY
+    val = numbers.concat(numbers2);
+
+
+    // Output
+    console.log(numbers);
+    console.log(val);
